916279454c3d3395449848270e6397e37e37a25a
Joshua Swiss <swissjoshua@gmail.com>
2015-09-07 18:51:29 +0100

    added a little css, did event listeners in jquery

--- js/jq-spotify.js
+++ js/jq-spotify.js
@@ -5,20 +5,28 @@ $(document).ready(function() {
   var inputSearch = $('#search-music');
   var searchSelect = $('#select-search');
   var results = $('#results');
+  var searchInput;
 
   //Event listeners
   // submitButton.on('click', getSpotifyData)
-  inputSearch.on('change', getSearchInput)
-  searchSelect.on('change', getSearchType)
+  inputSearch.on('change', getSearchInput);
+  searchSelect.on('change', getResults);
 
-  function getSearchType(e) {
+  function getSearchInput() {
+    searchInput = inputSearch.val().toLowerCase();
+  }
+
+  function getResults(e) {
     var searchType = $(this).val();
-    console.log(searchType);
+    console.log('Search type = ' + searchType);
+
+    $.get('https://api.spotify.com/v1/search?q=' + searchInput + '&type=' + searchType, function(response) {
+      console.log(response[searchType+'s']['items']);
+
+      var 
+    });
   }
 
-  function getSearchInput(e) {
-    var searchInput = $(this).val().toLowerCase();
-    console.log(searchInput);
-  }
+
 
 });
