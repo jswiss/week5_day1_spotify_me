8dab077633802ed3fa287744054b494c90d29193
Joshua Swiss <swissjoshua@gmail.com>
2015-09-07 19:20:46 +0100

    button now working

--- js/jq-spotify.js
+++ js/jq-spotify.js
@@ -1,32 +1,38 @@
 $(document).ready(function() {
 
   //define variables
-  var submitButton = $('#submit-search');  
   var inputSearch = $('#search-music');
   var searchSelect = $('#select-search');
   var results = $('#results');
+  var button = $('#submit');
   var searchInput;
 
+
   //Event listeners
-  // submitButton.on('click', getSpotifyData)
   inputSearch.on('change', getSearchInput);
   searchSelect.on('change', getResults);
+  button.on('click', getResults);
 
   function getSearchInput() {
     searchInput = inputSearch.val().toLowerCase();
   }
 
   function getResults(e) {
-    var searchType = $(this).val();
+    
+    event.preventDefault();
+    results.empty();
+
+    var searchType = $('#select-search').val();
     console.log('Search type = ' + searchType);
 
     $.get('https://api.spotify.com/v1/search?q=' + searchInput + '&type=' + searchType, function(response) {
-      console.log(response[searchType+'s']['items']);
+      
+      var music = response[searchType+'s']['items'];
 
-      var 
-    });
-  }
-
-
-
+      $.each(music, function(index, item) {
+        console.log(music[index]['name']);
+        results.append("<p>" + music[index]['name'] + "</p>")
+      });
+    })
+  };
 });
